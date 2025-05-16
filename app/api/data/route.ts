8dea71552f82a7d0ff05aa6c508a146c25064076
VathanakSol <vathanaksol1605@gmail.com>
2025-05-16 10:46:02 +0700

    first init

---
+++ app/api/data/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    
+    const data = {
+        message: 'Hello from the Cloudinator Workers API',
+        timestamp: new Date().toISOString(),
+    };
+
+    return NextResponse.json(data, {
+        headers: {
+            'Cache-Control': 's-maxage=3600, stale-while-revalidate',
+        }
+    })
+}
